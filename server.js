e6dbdb9d55a9e5b60aa27d6f4cbdffe99ae4918b
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-07-01 21:48:47 +0800

    installed nodemon - step 5

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.send('Hello from Express (using nodemon)!');
 });
 
 app.listen(PORT, () => {
