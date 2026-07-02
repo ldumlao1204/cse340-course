15a5fffdffebd091aeb481e63872ccca0b9941a2
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-07-02 21:57:32 +0800

    completed ejs templates - steps 1-6

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
 /**
   * Configure Express middleware
   */
@@ -21,19 +27,24 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+    const title = 'Home';
+    res.render('home', { title });
 });
 
 app.get('/organizations', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+    const title = 'Organizations';
+    res.render('organizations', { title });
 });
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+    const title = 'Projects';
+    res.render('projects', { title });
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
 });
+
