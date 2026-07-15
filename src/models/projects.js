820f288836f686883c2ed230eb77de7cdfc772cd
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-07-16 01:07:07 +0800

    completed route and query parameters activity

--- src/models/projects.js
+++ src/models/projects.js
@@ -21,4 +21,26 @@ const getAllProjects = async () => {
     return result.rows;
 }
 
-export { getAllProjects };
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+           sp.project_id,
+            sp.title,
+            sp.description,
+            sp.location,
+            sp.project_date,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM public.service_project sp
+        JOIN public.organization o
+            ON sp.organization_id = o.organization_id
+        WHERE sp.organization_id = $1
+        ORDER BY sp.project_date;
+    `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+export { getAllProjects, getProjectsByOrganizationId};
