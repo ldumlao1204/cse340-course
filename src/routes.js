b08d0154efaf53977e162563db89b7a77a437773
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-07-17 23:30:16 +0800

    fixed errors displaying service errors

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 import express from 'express';
 
 import { showHomePage } from './controllers/index.js';
-import { showOrganizationsPage } from './controllers/organizations.js';
-import { showProjectsPage } from './controllers/projects.js';
+import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
-import { showOrganizationDetailsPage } from './controllers/organizations.js';
-import { showProjectDetailsPage } from './controllers/projects.js';
+import { showOrganizationsPage, showOrganizationDetailsPage } from './controllers/organizations.js';
 
 const router = express.Router();
 
@@ -15,7 +13,7 @@ router.get('/organizations', showOrganizationsPage);
 router.get('/projects', showProjectsPage);
 router.get('/categories', showCategoriesPage);
 
-// error-handling routes
+// Error-handling routes
 router.get('/test-error', testErrorPage);
 
 // Route for organization details page
